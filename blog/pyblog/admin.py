337accebf2992fedc51590a49d96c4c424580831
Christopher Aguilar <chris.naibaf@gmail.com>
2022-09-02 11:35:55 -0500

    Registered Article and Comment models to display in admin page

--- blog/pyblog/admin.py
+++ blog/pyblog/admin.py
@@ -1,3 +1,7 @@
+from atexit import register
 from django.contrib import admin
+from .models import Article, Comment
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
