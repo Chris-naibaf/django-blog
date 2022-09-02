7d39cb342d69c91485696808311a27955cde8c8a
Christopher Aguilar <chris.naibaf@gmail.com>
2022-09-02 10:40:17 -0500

    Modified gitignore, created Article and comment models and, made the migrations and migrated the changes to the database

--- blog/pyblog/models.py
+++ blog/pyblog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('Date published')
+    likes = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    related_article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=500)
+    likes = models.IntegerField(default=0)
