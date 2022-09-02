0777f2d6323ddc0b2322d4e300b8adc8ce2f08a1
Christopher Aguilar <chris.naibaf@gmail.com>
2022-09-02 11:25:48 -0500

    Created an Article and comment using Django's python shell, added correct string representation

--- blog/pyblog/models.py
+++ blog/pyblog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
+    def __str__(self):
+        return self.article_title
+
     article_title = models.CharField(max_length=50, default="Article title")
     article_text = models.CharField(max_length=500)
     pub_date = models.DateTimeField('Date published')
@@ -9,6 +12,9 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
+    def __str__(self):
+        return self.comment_text
+
     related_article = models.ForeignKey(Article, on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=500)
     likes = models.IntegerField(default=0)
